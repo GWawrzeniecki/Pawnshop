2186b074f09f823aa505e4566c652e3d4b095376
Grzegorz Wawrzeniecki <grzegorz.wawrzeniecki@interia.pl>
2020-12-26 20:54:21 +0100

    Prepapring the solution 0.1

--- Pawnshop/Pawnshop.TestingApp/Program.cs
+++ Pawnshop/Pawnshop.TestingApp/Program.cs
@@ -7,10 +7,10 @@ namespace Pawnshop.TestingApp
     {
         static void Main(string[] args)
         {
-            using DAL.Data.UnitOfWork unitOfWork = new DAL.Data.UnitOfWork();
-            Console.WriteLine(unitOfWork.ClientRepository.Get().Count());      
             
 
+            int? test = null;
+            Console.WriteLine(test == null);
 
         }
     }
